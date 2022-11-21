ff8ade2c3ff0ccab7f18b50080c247e997a1e5cf
SilvinaMoreno <silvina.moreno@fi.uncoma.edu.ar>
2022-11-21 18:17:15 -0300

    agregué una linea de codigo en wordix.php

--- wordix.php
+++ wordix.php
@@ -36,7 +36,7 @@ const ESTADO_LETRA_PERTENECE = "pertenece";
 function solicitarNumeroEntre($min, $max)
 {
     //int $numero
-    echo "\nEliga un numero entre ".$min." y ".$max." : ";
+    echo "\nEliga un opcion entre ".$min." y ".$max." : "; //Agregué una linea de interacción con el usuario
     $numero = trim(fgets(STDIN));
     while (!is_int($numero) && !($numero >= $min && $numero <= $max)) {
         echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
